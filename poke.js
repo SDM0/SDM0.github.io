8b53af6281e61e48f0719512a46aeac314859ab3
SDM0 <60699143+SDM0@users.noreply.github.com>
2020-10-05 23:32:34 +0200

    New website: Pokecalc Fusion

---
+++ poke.js
@@ -0,0 +1,25 @@
+var poke = require('poke.json');
+console.log(poke.bulbasaur);
+
+var mon1 = (document.getElementById("pokemon1")).value;
+var mon2 = (document.getElementById("pokemon2")).value;
+document.getElementById(("FP1")).append(" "+mon1+"/"+mon2);
+document.getElementById(("FP2")).append(" "+mon2+"/"+mon1);
+
+document.getElementById("pokemon1").onchange= function() {change()};
+document.getElementById("pokemon2").onchange= function() {change()};
+
+function change() {
+    var mon1 = (document.getElementById("pokemon1")).value;
+    var mon2 = (document.getElementById("pokemon2")).value;
+    console.log(mon1);
+    console.log(json.bulbasaur);
+    if (mon1!=mon2) {
+        console.log(library.includes(mon1));
+
+
+        console.log(document.getElementById(("FP1")).value);
+        document.getElementById(("FP1")).innerHTML=(" "+mon1+"/"+mon2);
+        document.getElementById(("FP2")).innerHTML=(" "+mon2+"/"+mon1);
+    }
+}
