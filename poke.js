8a73736a0ae79033980f1c1f48a179f9853a5e0d
SDM0 <60699143+SDM0@users.noreply.github.com>
2020-10-14 17:58:56 +0200

    Update types

--- poke.js
+++ poke.js
@@ -1,4 +1,6 @@
-ids= [["Azurill",252]
+ids= 
+[["Slowking",199]
+,["Azurill",252]
 ,["Wynaut",253]
 ,["Ambipom",254]
 ,["Mismagius",255]
@@ -168,6 +170,35 @@ ids= [["Azurill",252]
 ,["Larvesta",419]
 ,["Stunfisk",420]]
 
+typeSwap=
+[["steel","electric","Magnemite"]
+,["steel","electric","Magneton"]
+,["ice","water","Dewgong"]
+,["water","rock","Omanyte"]
+,["water","rock","Omastar"]
+,["steel","bug","Scizor"]
+,["steel","electric","Magnezone"]]
+
+typeUni=
+[["grass","Bulbasaur"]
+,["grass","Ivysaur"]
+,["grass","Venusaur"]
+,["fire","Charizard"]
+,["rock","Geodude"]
+,["rock","Graveler"]
+,["rock","Golem"]
+,["ghost","Gastly"]
+,["ghost","Haunter"]
+,["ghost","Gengar"]
+,["rock","Onix"]
+,["bug","Scyther"]
+,["water","Gyarados"]
+,["ice","Articuno"]
+,["electric","Zapdos"]
+,["fire","Moltres"]
+,["dragon","Dragonite"]
+,["steel","Steelix"]]
+
 var pkmn1 = document.getElementById('fname1');
 pkmn1.addEventListener("keydown",function(event) {
     if (event.keyCode===13) {
@@ -195,6 +226,11 @@ function selectPoke() {
         } else if (mon2=="giratina") {
             mon2="giratina-altered";
         }
+        if (mon1=="deoxys") {
+            mon1="deoxys-normal";
+        } else if (mon2=="deoxys") {
+            mon2="deoxys-normal";
+        }
         var txhr = new XMLHttpRequest();  
         var poke1 = 'https://pokeapi.co/api/v2/pokemon/'+mon1
         txhr.open('GET', poke1, true);
@@ -204,11 +240,39 @@ function selectPoke() {
         var jsonString = JSON.parse(jsonBody);
         var num1 = jsonString.id;
         for (i=0;i<ids.length;i++){
-            console.log(ids[i][0],mon1);
             if (ids[i][0]==mon1.charAt(0).toUpperCase() + mon1.slice(1)) {
                 num1=ids[i][1];
             }
         }
+        var type1 = jsonString.types;
+        var mon1types = [];
+        var compt=0
+        for (i=0;i<typeSwap.length;i++) {
+            if (typeSwap[i][2]==mon1.charAt(0).toUpperCase() + mon1.slice(1)) {
+                mon1types.push(typeSwap[i][0]);
+                mon1types.push(typeSwap[i][1]);
+                var compt=1;
+            }
+        }
+
+        for (i=0;i<typeUni.length;i++) {
+            if (typeUni[i][1]==mon1.charAt(0).toUpperCase() + mon1.slice(1)) {
+                mon1types.push(typeUni[i][0]);
+                var compt=2;
+            }
+        }
+
+        if (compt==0) {
+            mon1types.push(type1[0].type.name);
+            if (type1.length==2 && compt!=2) {
+                if (type1[0].type.name=="normal" && type1[1].type.name=="flying") {
+                    mon1types[0]="flying";
+                } else {
+                    mon1types.push(type1[1].type.name);
+                }
+            }
+        }
+        console.log(mon1types);
         var stats1 = jsonString.stats;
         var mon1stats = [];
         for (i=0; i<stats1.length;i++) {
@@ -220,30 +284,68 @@ function selectPoke() {
         pxhr.open('GET', poke2, true);
         pxhr.send();
         pxhr.onload = function() {
-            if (mon1!="giratina-altered" && mon2!="giratina-altered") {
+            if (mon1!="giratina-altered" && mon2!="giratina-altered" && mon1!="deoxys-normal" && mon2!="deoxys-normal") {
             var fmon1 = mon1.charAt(0).toUpperCase() + mon1.slice(1);
             var fmon2 = mon2.charAt(0).toUpperCase() + mon2.slice(1);
-            } else if (mon1=="giratina-altered") {
+            }
+            if (mon1=="giratina-altered") {
                 var fmon1 = "Giratina";
                 var fmon2 = mon2.charAt(0).toUpperCase() + mon2.slice(1);
-            } else if (mon2=="giratina-altered") {
+            }
+            if (mon2=="giratina-altered") {
                 var fmon1 = mon1.charAt(0).toUpperCase() + mon1.slice(1);
                 var fmon2 = "Giratina"
             }
-            document.getElementById("FP1").innerHTML=fmon1+"/"+fmon2;
-            document.getElementById("FP2").innerHTML=fmon2+"/"+fmon1;
+            if (mon1=="deoxys-normal") {
+                var fmon1 = "Deoxys";
+                var fmon2 = mon2.charAt(0).toUpperCase() + mon2.slice(1);
+            }
+            if (mon2=="deoxys-normal") {
+                var fmon1 = mon1.charAt(0).toUpperCase() + mon1.slice(1);
+                var fmon2 = "Deoxys"
+            }
             var jsonBody = pxhr.responseText;
             var jsonString = JSON.parse(jsonBody);
             var num2 = jsonString.id;
             for (i=0;i<ids.length;i++){
-                console.log(ids[i][0],mon2);
                 if (ids[i][0]==mon2.charAt(0).toUpperCase() + mon2.slice(1)) {
                     num2=ids[i][1];
                 }
             }
+            document.getElementById("FP1").innerHTML=fmon1+"/"+fmon2;
+            document.getElementById("FP2").innerHTML=fmon2+"/"+fmon1;
             var pic1 = num1+"."+num2+".png";
             var pic2 = num2+"."+num1+".png";
 
+            var type2 = jsonString.types;
+            var mon2types = [];
+            var compt=0
+            for (i=0;i<typeSwap.length;i++) {
+                if (typeSwap[i][2]==mon2.charAt(0).toUpperCase() + mon2.slice(1)) {
+                    mon2types.push(typeSwap[i][0]);
+                    mon2types.push(typeSwap[i][1]);
+                    var compt=1;
+                }
+            }
+    
+            for (i=0;i<typeUni.length;i++) {
+                if (typeUni[i][1]==mon2.charAt(0).toUpperCase() + mon2.slice(1)) {
+                    mon2types.push(typeUni[i][0]);
+                    var compt=2;
+                }
+            }
+    
+            if (compt==0) {
+                mon2types.push(type2[0].type.name);
+                if (type2.length==2 && compt!=2) {
+                    if (type2[0].type.name=="normal" && type2[1].type.name=="flying") {
+                        mon2types[0]="flying";
+                    } else {
+                        mon2types.push(type2[1].type.name);
+                    }
+                }
+            }
+            console.log(mon2types);
             var stats2 = jsonString.stats;
             var mon2stats = [];
             for (i=0; i<stats2.length;i++) {
@@ -260,8 +362,8 @@ function selectPoke() {
                     abs.push();
                 }
             }
-            document.getElementById("ab").innerHTML="Possible abilites: "+abs;
-            document.getElementById("lien").innerHTML="How to know the abilities?";
+            //document.getElementById("ab").innerHTML="Possible abilites: "+abs;
+            //document.getElementById("lien").innerHTML="How to know the abilities?";
             
             var hp1 = (mon2stats[0]/3)+2*(mon1stats[0]/3);
             var atk1 = 2*(mon2stats[1]/3)+(mon1stats[1]/3);
@@ -315,8 +417,28 @@ function selectPoke() {
             document.getElementById("spe2").innerHTML="SPEED: "+Math.floor(spe2);
             document.getElementById("bs2").innerHTML="TOTAL: "+Math.floor(bs2);
 
-            console.log(pic1);
-            console.log(pic2);
+            var fmonres1 = fusType(mon1types,mon2types);
+            var fmonres2 = fusType(mon2types,mon1types);
+            
+            console.log("fmonres1:",fmonres1);
+            console.log("fmonres2:",fmonres2);
+
+            document.getElementById("p1").src="./Types/"+fmonres1[0]+".png";
+            if (fmonres1.length!=1 && (fmonres1.length==2 && fmonres1[0]!=fmonres1[1])) {
+                document.getElementById("p2").style.display="inline-block";
+                document.getElementById("p2").src="./Types/"+fmonres1[1]+".png";
+            } else {
+                document.getElementById("p2").style.display="none";
+            }
+
+            document.getElementById("p3").src="./Types/"+fmonres2[0]+".png";
+
+            if (fmonres2.length!=1 && (fmonres2.length==2 && fmonres2[0]!=fmonres2[1])) {
+                document.getElementById("p4").style.display="inline-block";
+                document.getElementById("p4").src="./Types/"+fmonres2[1]+".png";
+            } else {
+                document.getElementById("p4").style.display="none";
+            }
 
             document.getElementById("pic1").src="./CustomBattlers/"+pic1;
             document.getElementById("pic2").src="./CustomBattlers/"+pic2;
@@ -324,4 +446,51 @@ function selectPoke() {
 
         }
     }
+}
+
+function fusType(mon1,mon2) {
+    //cas H1/null + B1/null [H1#B1] -> H1/B1
+    var fmon = []
+    if (mon1.length==1 && mon2.length==1) {
+        if (mon1[0]!=mon2[0]) {
+            fmon.push(mon1[0]);
+            fmon.push(mon2[0])
+    //cas H1/null + B1/null [H1=B1] -> H1/null
+        } else {
+            fmon.push(mon1[0]);
+        }
+    } else if (mon1.length==2 && mon2.length==1) {
+    //cas H1/H2 + B1/null [H1#B1] -> H1/B1
+        if (mon1[0]!=mon2[0]) {
+            fmon.push(mon1[0]);
+            fmon.push(mon2[0]);
+    //cas H1/H2 + B1/null [H1=B1] -> H1/H2
+        } else {
+            fmon.push(mon1[0]);
+            fmon.push(mon1[1]);
+        }
+    } else if (mon1.length==1 && mon2.length==2) {
+    //cas H1/null + B1/B2 [H1#B1] -> H1/B1
+            fmon.push(mon1[0]);
+            fmon.push(mon2[0]);
+    } else if (mon1.length==2 && mon1.length==1) {
+    //cas H1/H2 + B1/B2 [H1#B2] -> H1/B2
+        if (mon1[0]!=mon2[1]) {
+            fmon.push(mon1[0]);
+            fmon.push(mon2[1]);
+    //cas H1/H2 + B1/B2 [H1=B2] -> H1/B1
+        } else {
+            fmon.push(mon1[0]);
+            fmon.push(mon2[0]);
+        }
+    } else if (mon1.length==2 && mon1.length==2) {
+        if (mon1[0]!=mon2[1]) {
+            fmon.push(mon1[0]);
+            fmon.push(mon2[1]);
+        } else {
+            fmon.push(mon1[0]);
+            fmon.push(mon1[1]);
+        }
+    }
+    return fmon
 }
