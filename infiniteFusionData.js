55d81098b055a2cfb348a4de7978aebcaebc0b2e
Aegide <geoffroygressier1@yahoo.fr>
2021-02-23 19:57:19 +0100

    Merge commit 'b36d8b95d33932aea876f753b2a0580767f9263d'

---
+++ infiniteFusionData.js
@@ -0,0 +1,703 @@
+// ##### Stats 
+
+//Aegislash
+const aegislashStats = [
+    {"base_stat": 60},
+    {"base_stat": 50},
+    {"base_stat": 150},
+    {"base_stat": 50},
+    {"base_stat": 150},
+    {"base_stat": 60}
+]
+
+//Butterfree
+const butterfreeStats = [
+    {"base_stat": 60},
+    {"base_stat": 45},
+    {"base_stat": 50},
+    {"base_stat": 80},
+    {"base_stat": 80},
+    {"base_stat": 70}
+]
+
+//Beedrill
+const beedrillStats = [
+    {"base_stat": 65},
+    {"base_stat": 80},
+    {"base_stat": 40},
+    {"base_stat": 45},
+    {"base_stat": 80},
+    {"base_stat": 75}
+]
+
+//Pidgeot
+const pidgeotStats = [
+    {"base_stat": 83},
+    {"base_stat": 80},
+    {"base_stat": 75},
+    {"base_stat": 70},
+    {"base_stat": 70},
+    {"base_stat": 91}
+]
+
+//Pikachu
+const pikachuStats = [
+    {"base_stat": 35},
+    {"base_stat": 55},
+    {"base_stat": 30},
+    {"base_stat": 50},
+    {"base_stat": 40},
+    {"base_stat": 90}
+]
+
+//Raichu
+const raichuStats = [
+    {"base_stat": 60},
+    {"base_stat": 90},
+    {"base_stat": 55},
+    {"base_stat": 90},
+    {"base_stat": 80},
+    {"base_stat": 100}
+]
+
+//Nidoqueen
+const nidoqueenStats = [
+    {"base_stat": 90},
+    {"base_stat": 82},
+    {"base_stat": 87},
+    {"base_stat": 75},
+    {"base_stat": 85},
+    {"base_stat": 76}
+]
+
+//Nidoking
+const nidokingStats = [
+    {"base_stat": 81},
+    {"base_stat": 92},
+    {"base_stat": 77},
+    {"base_stat": 85},
+    {"base_stat": 75},
+    {"base_stat": 85}
+]
+
+//Clefable
+const clefableStats = [
+    {"base_stat": 95},
+    {"base_stat": 70},
+    {"base_stat": 73},
+    {"base_stat": 85},
+    {"base_stat": 90},
+    {"base_stat": 60}
+]
+
+//Wigglytuff
+const wigglytuffStats = [
+    {"base_stat": 140},
+    {"base_stat": 70},
+    {"base_stat": 45},
+    {"base_stat": 75},
+    {"base_stat": 50},
+    {"base_stat": 45}
+]
+
+//Vileplume
+const vileplumeStats = [
+    {"base_stat": 75},
+    {"base_stat": 80},
+    {"base_stat": 85},
+    {"base_stat": 100},
+    {"base_stat": 90},
+    {"base_stat": 50}
+]
+
+//Poliwrath
+const poliwrathStats = [
+    {"base_stat": 90},
+    {"base_stat": 85},
+    {"base_stat": 95},
+    {"base_stat": 70},
+    {"base_stat": 90},
+    {"base_stat": 70}
+]
+
+//Alakazam
+const alakazamStats = [
+    {"base_stat": 55},
+    {"base_stat": 50},
+    {"base_stat": 45},
+    {"base_stat": 135},
+    {"base_stat": 85},
+    {"base_stat": 120}
+]
+
+//Victreebel
+const victreebelStats = [
+    {"base_stat": 80},
+    {"base_stat": 105},
+    {"base_stat": 65},
+    {"base_stat": 100},
+    {"base_stat": 60},
+    {"base_stat": 70}
+]
+
+//Golem
+const golemStats = [
+    {"base_stat": 80},
+    {"base_stat": 110},
+    {"base_stat": 130},
+    {"base_stat": 55},
+    {"base_stat": 65},
+    {"base_stat": 45}
+]
+
+//Ampharos
+const ampharosStats = [
+    {"base_stat": 90},
+    {"base_stat": 75},
+    {"base_stat": 75},
+    {"base_stat": 115},
+    {"base_stat": 90},
+    {"base_stat": 55}
+]
+
+//Bellossom
+const bellossomStats = [
+    {"base_stat": 75},
+    {"base_stat": 80},
+    {"base_stat": 85},
+    {"base_stat": 90},
+    {"base_stat": 100},
+    {"base_stat": 50}
+]
+
+//Azumarill
+const azumarillStats = [
+    {"base_stat": 100},
+    {"base_stat": 50},
+    {"base_stat": 80},
+    {"base_stat": 50},
+    {"base_stat": 80},
+    {"base_stat": 50}
+]
+
+//Jumpluff
+const jumpluffStats = [
+    {"base_stat": 75},
+    {"base_stat": 55},
+    {"base_stat": 70},
+    {"base_stat": 55},
+    {"base_stat": 85},
+    {"base_stat": 110}
+]
+
+//Roserade
+const roseradeStats = [
+    {"base_stat": 60},
+    {"base_stat": 70},
+    {"base_stat": 55},
+    {"base_stat": 125},
+    {"base_stat": 105},
+    {"base_stat": 100}
+]
+
+//Krookodile
+const krookodileStats = [
+    {"base_stat": 95},
+    {"base_stat": 117},
+    {"base_stat": 70},
+    {"base_stat": 65},
+    {"base_stat": 70},
+    {"base_stat": 92}
+]
+
+
+
+// ##### Abilities 
+
+//Chandelure
+const chandelureAbilities = [
+    {
+        "ability": {
+            "name": "flash-fire"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "flame-body",
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "shadow-tag",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Lampent, 
+const lampentAbilities = chandelureAbilities;
+
+//Litwick
+const litwickAbilities = chandelureAbilities;
+
+//Wigglytuff
+const wigglytuffAbilities = [
+    {
+        "ability": {
+            "name": "cute-charm"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "frisk",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Jigglypuff
+const jigglypuffAbilities = [
+    {
+        "ability": {
+            "name": "cute-charm"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "friend-guard",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Igglybuff
+const igglybuffAbilities = jigglypuffAbilities;
+
+//Zapdos
+const zapdosAbilities = [
+    {
+        "ability": {
+            "name": "pressure"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "lightning-rod",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Raikou
+const raikouAbilities = [
+    {
+        "ability": {
+            "name": "pressure"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "volt-absorb",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Entei
+const enteiAbilities = [
+    {
+        "ability": {
+            "name": "pressure"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "flash-fire",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Suicune
+const suicuneAbilities = [
+    {
+        "ability": {
+            "name": "pressure"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "water-absorb",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Milotic
+const miloticAbilities = [
+    {
+        "ability": {
+            "name": "marvel-scale"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "cute-charm",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Feebas
+const feebasAbilities = [
+    {
+        "ability": {
+            "name": "swift-swim"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "adaptability",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Gengar
+const gengarAbilities = [
+    {
+        "ability": {
+            "name": "levitate"
+        },
+        "is_hidden": false,
+    },
+]
+
+//Weezing
+const weezingAbilities = [
+    {
+        "ability": {
+            "name": "levitate"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "stench",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Koffing
+const koffingAbilities = weezingAbilities;
+
+//Mewtwo
+const mewtwoAbilities = [
+    {
+        "ability": {
+            "name": "pressure"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "immunity",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Unown
+const unownAbilities = [
+    {
+        "ability": {
+            "name": "levitate"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "mummy",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Flygon
+const flygonAbilities = [
+    {
+        "ability": {
+            "name": "levitate"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "dry-skin",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Regigigas
+const regigigasAbilities = [
+    {
+        "ability": {
+            "name": "slow-start"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "mold-breaker",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Darkrai
+const darkraiAbilities = [
+    {
+        "ability": {
+            "name": "bad-dreams"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "white-smoke",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Genesect
+const genesectAbilities = [
+    {
+        "ability": {
+            "name": "download"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "motor-drive",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Reshiram
+const reshiramAbilities = [
+    {
+        "ability": {
+            "name": "turboblaze"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "flare-boost",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Zekrom
+const zekromAbilities = [
+    {
+        "ability": {
+            "name": "teravolt"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "volt-absorb",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Kyurem
+const kyuremAbilities = [
+    {
+        "ability": {
+            "name": "pressure"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "ice-body",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Ferrothorn
+const ferrothornAbilities = [
+    {
+        "ability": {
+            "name": "iron-barbs"
+        },
+        "is_hidden": false,
+    },
+]
+
+//Talonflame
+const talonflameAbilities = [
+    {
+        "ability": {
+            "name": "big-pecks"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "gale-wings",
+        },
+        "is_hidden": true,
+    }
+]
+
+//Hydreigon
+const hydreigonAbilities = [
+    {
+        "ability": {
+            "name": "levitate"
+        },
+        "is_hidden": false,
+    },
+    {
+        "ability": {
+            "name": "hustle",
+        },
+        "is_hidden": true,
+    }
+]
+
+
+
+
+
+
+
+// ##### Tables
+
+const abilitiesException = [
+    "chandelure",
+    "lampent",
+    "litwick",
+    "wigglytuff",
+    "jigglypuff",
+    "igglybuff",
+    "zapdos",
+    "raikou",
+    "entei",
+    "suicune",
+    "milotic",
+    "feebas",
+    "gengar",
+    "weezing",
+    "koffing",
+    "mewtwo",
+    "unown",
+    "flygon",
+    "regigigas",
+    "darkrai",
+    "genesect",
+    "reshiram",
+    "zekrom",
+    "kyurem",
+    "ferrothorn",
+    "talonflame",
+    "hydreigon"
+]
+
+const abilitiesFix = [
+    chandelureAbilities,
+    lampentAbilities,
+    litwickAbilities,
+    wigglytuffAbilities,
+    jigglypuffAbilities,
+    igglybuffAbilities,
+    zapdosAbilities,
+    raikouAbilities,
+    enteiAbilities,
+    suicuneAbilities,
+    miloticAbilities,
+    feebasAbilities,
+    gengarAbilities,
+    weezingAbilities,
+    koffingAbilities,
+    mewtwoAbilities,
+    unownAbilities,
+    flygonAbilities,
+    regigigasAbilities,
+    darkraiAbilities,
+    genesectAbilities,
+    reshiramAbilities,
+    zekromAbilities,
+    kyuremAbilities,
+    ferrothornAbilities,
+    talonflameAbilities,
+    hydreigonAbilities
+]
+
+const statsException = [
+    "aegislash-shield",
+    "butterfree",
+    "beedrill",
+    "pidgeot",
+    "pikachu",
+    "raichu",
+    "nidoqueen",
+    "nidoking",
+    "clefable",
+    "wigglytuff",
+    "vileplume",
+    "poliwrath",
+    "alakazam",
+    "victreebel",
+    "golem",
+    "ampharos",
+    "bellossom",
+    "azumarill",
+    "jumpluff",
+    "roserade"
+]
+
+const statsFix = [
+    aegislashStats,
+    butterfreeStats,
+    beedrillStats,
+    pidgeotStats,
+    pikachuStats,
+    raichuStats,
+    nidoqueenStats,
+    nidokingStats,
+    clefableStats,
+    wigglytuffStats,
+    vileplumeStats,
+    poliwrathStats,
+    alakazamStats,
+    victreebelStats,
+    golemStats,
+    ampharosStats,
+    bellossomStats,
+    azumarillStats,
+    jumpluffStats,
+    roseradeStats
+]
