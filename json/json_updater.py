218f69ddf1ba6419e580b949db936f0373feba3d
Aegide <geoffroygressier1@yahoo.fr>
2021-07-25 13:42:46 +0200

    Update json updater

--- json/json_updater.py
+++ json/json_updater.py
@@ -2,16 +2,17 @@ from os import listdir
 from os.path import isfile, join
 import json
 
-mypath = "CustomBattlers"
+path_custom = "CustomBattlers"
+path_json = "json"
 fusions = []
 
-for element in listdir(mypath):
-    if isfile(join(mypath, element)) and element.endswith(".png"):
+for element in listdir(path_custom):
+    if isfile(join(path_custom, element)) and element.endswith(".png"):
         fusions.append(element[:-4])
         print(element[:-4])
 
 jsonStr = json.dumps(fusions)
-jsonFile = open(mypath + "/" +"sprites.json", "w")
+jsonFile = open(path_json + "/" +"sprites.json", "w")
 jsonFile.write(jsonStr)
 jsonFile.close()
 
